f1c33b5a5481e1082e2397e45593d2c564ab9de1
OUVC <143134439+ouvcs@users.noreply.github.com>
2024-01-03 20:51:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 10,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+fetch("./geos/peoples-republic-agaria.geojson").then((data) => data.json()).then((geo) => L.geoJSON(geo).addTo(map));
+fetch("./geos/severnaya-federation.geojson").then((data) => data.json()).then((geo) => L.geoJSON(geo).addTo(map));
