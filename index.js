d7d55009671ee4824bb02e4952653150a90cda02
OUVC <143134439+ouvcs@users.noreply.github.com>
2024-01-03 21:25:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let map = L.map("map").setView([0.0, 0.0], 1);
-//let citiesMarkers = new L.featureGroup();
+let citiesMarkers = new L.featureGroup();
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 10,
