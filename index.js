b4639428a854e2a118cd1a522836c195df00dab9
OUVC <143134439+ouvcs@users.noreply.github.com>
2024-01-03 21:31:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let map = L.map("map").setView([0.0, 0.0], 1);
 let citiesMarkers = new L.featureGroup();
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 10,
+    maxZoom: 7,
     attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`
 }).addTo(map);
 
@@ -11,17 +11,16 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 function geoJSON(geo) {
     L.geoJSON(geo, {
         pointToLayer: function (feature, latlng) {
-            //citiesMarkers.addLayer(feature);
+            citiesMarkers.addLayer(feature);
             return L.circleMarker(latlng, {radius: 3, fillColor: "#FFFFFF", color: "#000000", weight: 1, opacity: 1, fillOpacity: 1});
         }
     }).addTo(map);
 }
 
 map.on("zoomend", function() {
-    if (map.getZoom() < 7){
+    if (map.getZoom() >= 3){
         map.removeLayer(citiesMarkers);
-    }
-    else {
+    } else {
         map.addLayer(citiesMarkers);
     }
 });
