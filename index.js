e923ea1bff9bb98df792791f9cb01941822750dd
OUVC <143134439+ouvcs@users.noreply.github.com>
2024-01-03 20:47:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+var map = L.map('map');
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 10,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
