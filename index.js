8050af4542edf37c0acd89faf8df9d6d3fc60988
OUVC <143134439+ouvcs@users.noreply.github.com>
2024-01-03 20:55:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var map = L.map('map');
+var map = L.map("map");
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 10,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`
 }).addTo(map);
 
 fetch("./geos/peoples-republic-agaria.geojson").then((data) => data.json()).then((geo) => L.geoJSON(geo).addTo(map));
