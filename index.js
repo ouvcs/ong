d48ffbd36c91db00781adfeb5254afab92e5b34b
OUVC <143134439+ouvcs@users.noreply.github.com>
2024-01-03 21:15:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var map = L.map("map").setView([0.0, 0.0], 1);
+var citiesMarkers = new L.FeatureGroup();
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 10,
     attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`
 }).addTo(map);
 
+
+
 function geoJSON(geo) {
     L.geoJSON(geo, {
         pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, {radius: 8, fillColor: "#ff7800", color: "#000", weight: 1, opacity: 1, fillOpacity: 0.8});
+            citiesMarkers.addLayer(feature);
+            return L.circleMarker(latlng, {radius: 3, fillColor: "#FFFFFF", color: "#000000", weight: 1, opacity: 1, fillOpacity: 1});
         }
     }).addTo(map);
 }
+
+map.on('zoomend', function() {
+    if (map.getZoom() < 7){
+        map.removeLayer(citiesMarkers);
+    }
+    else {
+        map.addLayer(citiesMarkers);
+    }
+});
 
 fetch("./geos/peoples-republic-agaria.geojson").then((data) => data.json()).then((geo) => geoJSON(geo));
 fetch("./geos/severnaya-federation.geojson").then((data) => data.json()).then((geo) => geoJSON(geo));
