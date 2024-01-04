f6fd1c37ad1cd9a335956e834854a5b62f740361
OUVC <143134439+ouvcs@users.noreply.github.com>
2024-01-04 12:55:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ function geoJSON(geo, meta) {
     console.log("g0"+meta.name);
     L.geoJSON(geo, {
         pointToLayer: function (feature, latlng) {
-            let marker = L.circleMarker(latlng, {radius: 5, fillColor: "#FFFFFF", color: "#000000", weight: 1, opacity: 1, fillOpacity: 1});
+            let marker;
+            let type = feature.properties.type;
+            if (type == "capital") {
+                marker = L.circleMarker(latlng, {radius: 5, fillColor: "#FFFFFF", color: "#000000", weight: 2, opacity: 1, fillOpacity: 1});
+            else {
+                marker = L.circleMarker(latlng, {radius: 4, fillColor: "#FFFFFF", color: "#000000", weight: 1, opacity: 1, fillOpacity: 1});
+            }
             marker.bindPopup(function () {
-                let type = feature.properties.type;
                 if (type == "capital") {type = "Столица"} else {type = "Город"};
                 return `<div class="popup"><div class="flag"><img src="${meta.flag}" alt="FLAG"></div><div class="info"><div class="name"><strong>${type}</strong> ${feature.properties.name}</h3></div></div>`;
             });
